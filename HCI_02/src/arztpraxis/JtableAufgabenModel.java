782348cfb8c54198c2d2d299004390605dd2bdc5
niggli <roger.niggli@gmx.ch>
2014-04-06 11:55:46 +0200

    was vergässe

---
+++ HCI_02/src/arztpraxis/JtableAufgabenModel.java
@@ -0,0 +1,67 @@
+package arztpraxis;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Vector;
+
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+
+public class JtableAufgabenModel implements TableModel {
+	private Vector listenersVector = new Vector();
+	Calendar firstWeekDay = Calendar.getInstance();
+	int index = 1;
+
+	public JtableAufgabenModel(){
+		
+	}
+	
+	public int getColumnCount() {
+		return 1;
+	}
+
+	public void addAufgabe() {
+		index++;
+	}
+
+	public int getRowCount() {
+		// return personVector.size();
+		return index;
+	}
+
+	public String getColumnName(int column) {
+
+		return "asdf";
+
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		return "";
+	}
+
+	public Class getColumnClass(int columnIndex) {
+		return String.class;
+	}
+
+	public void setFirtWeekDay(Calendar firstWeekDay) {
+		this.firstWeekDay = firstWeekDay;
+	}
+
+	public void addTableModelListener(TableModelListener l) {
+		listenersVector.add(l);
+	}
+
+	public void removeTableModelListener(TableModelListener l) {
+		listenersVector.remove(l);
+	}
+
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return true;
+	}
+
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		// nicht beachten
+	}
+}
